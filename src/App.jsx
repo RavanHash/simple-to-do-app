f74a3eda2c7616bc223f155bd2b58ba6357eb6ca
Ravan Hashimov <ravanhashimov@gmail.com>
2023-04-29 22:28:45 +0300

    Add submit task and toggle task

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,64 @@
 import "./style.css";
+import { useState } from "react";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
   return (
     <>
-      <form className={"new-item-form"}>
+      <form onSubmit={handleSubmit} className={"new-item-form"}>
         <div className="form-row">
           <label htmlFor="item">New uwu Task</label>
-          <input type="text" id="item" />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            id="item"
+          />
           <button className="btn">+</button>
         </div>
       </form>
       <h1 className="header"> ToDo uwu List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            uwu 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
