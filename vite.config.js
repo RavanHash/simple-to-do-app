d8d349ab89dbd15c0ed63afb6c325bdf35891b93
Ravan Hashimov <ravanhashimov@gmail.com>
2023-05-02 13:40:55 +0300

    deploy 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/simple-to-do-app/",
   plugins: [react()],
+  base: "/simple-to-do-app/",
 });
